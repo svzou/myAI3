7859e0971afae9ef409e67a7e3dee5f540d73423
svzou <111460074+svzou@users.noreply.github.com>
2026-03-31 20:10:07 -0400

    Refactor prompts by removing course context and updating tool calling
    Removed COURSE_CONTEXT_PROMPT and updated TOOL_CALLING_PROMPT to prioritize vector database retrieval without web search.

--- prompts.ts
+++ prompts.ts
@@ -7,7 +7,7 @@ You are ${AI_NAME}, an agentic assistant. You are designed by ${OWNER_NAME}, not
 
 export const TOOL_CALLING_PROMPT = `
 - In order to be as truthful as possible, call tools to gather context before answering.
-- Prioritize retrieving from the vector database, and then the answer is not found, search the web.
+- Retrieve from the vector database, do not search the web.
 `;
 
 export const TONE_STYLE_PROMPT = `
@@ -42,10 +42,6 @@ export const CITATIONS_PROMPT = `
 - Do not ever just use [Source #] by itself and not provide the URL as a markdown link-- this is forbidden.
 `;
 
-export const COURSE_CONTEXT_PROMPT = `
-- Most basic questions about the course can be answered by reading the syllabus.
-`;
-
 export const SYSTEM_PROMPT = `
 ${IDENTITY_PROMPT}
 
@@ -65,9 +61,6 @@ ${GUARDRAILS_PROMPT}
 ${CITATIONS_PROMPT}
 </citations>
 
-<course_context>
-${COURSE_CONTEXT_PROMPT}
-</course_context>
 
 <date_time>
 ${DATE_AND_TIME}
